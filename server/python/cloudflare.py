ed40886a65b4e8ee44e782d4748c7d68be3adaaa
Kipsanga-Gregory <greglimo366@yahoo.com>
2022-11-29 13:16:19 +0300

    yest

--- server/python/cloudflare.py
+++ server/python/cloudflare.py
@@ -7,9 +7,16 @@ import sys
 from time import sleep
 
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+s = webdriver.Chrome(chrome_options=chrome_options)
 
 
-s = Service(r'chromedriver.exe')        # <--- ENTER THE PATH OF CHROMEDRIVER.EXE HERE
+
+# s = Service(r'chromedriver.exe')        # <--- ENTER THE PATH OF CHROMEDRIVER.EXE HERE
 BASE_URL = 'https://abuse.cloudflare.com/phishing'
 
 
